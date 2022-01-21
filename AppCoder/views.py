f0de821eb153a263238973e29848dd6cc0990590
Agustina Tamborindegui <agustinailent@gmail.com>
2022-01-21 14:00:24 -0300

    usando templates

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,3 +1,26 @@
+from cgitb import html
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+from .models import Curso
+
+def crear_curso (request, camada):
+    curso= Curso (nombre='python', camada = camada)
+    curso.save
+    return HttpResponse(f'curso creado!{camada}')
+
+def inicio(request):
+    return render(request, 'AppCoder/inicio.html')
+    
+def curso(request):
+    return render(request, 'AppCoder/cursos.html')
+
+def profesores(request):
+    return HttpResponse('profesores')
+
+def estudiantes(request):
+    return HttpResponse('estudiantes')
+
+def entregables(request):
+    return HttpResponse('entregables')
